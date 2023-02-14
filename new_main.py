17fdab478992309ed226a15f8956c16ffa1ae836
terrabela <maduar@vivaldi.net>
2023-02-14 20:32:50 -0300

    Update to Qt5

--- new_main.py
+++ new_main.py
@@ -10,11 +10,18 @@ from PySide2 import QtWidgets
 # https://www.pythonguis.com/tutorials/pyside-creating-dialogs-qt-designer/
 
 # 2023-Fev-14 Parei aqui...
+# RESOLVER: usar resource compiler para compilar mdi.qrc em Qt5 (mdi_rc.py antigo
+# está em Qt4
 
-# from MainWindow import Ui_MainWindow
-from ui_new_main_ui import Ui_MainWindow
+# Para chamar a ui simplezinha, ui_new_main_ui
+# from ui_new_main_ui import Ui_MainWindow
 
-class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+# Para chamar a antiga e complexa interface, ui_main
+from ui_maininterface_with_toolbar import Ui_MainInterfaceClass
+
+# class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+class MainWindow(QtWidgets.QMainWindow, Ui_MainInterfaceClass):
+
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
