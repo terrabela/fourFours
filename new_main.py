718794425ac8201627029c19bde785c827b563e5
terrabela <maduar@vivaldi.net>
2023-02-16 20:09:31 -0300

    Struggling to conect the dialogs to the app...

--- new_main.py
+++ new_main.py
@@ -1,6 +1,5 @@
-
 import sys
-from PySide2 import QtWidgets
+from PySide2.QtWidgets import QApplication, QMainWindow, QMessageBox, QDialog
 
 # 2023-Fev-14
 # A organização dos tutoriais é algo confusa.
@@ -9,26 +8,53 @@ from PySide2 import QtWidgets
 # Em seguida, para adicionar diálogos ao app:
 # https://www.pythonguis.com/tutorials/pyside-creating-dialogs-qt-designer/
 
-# 2023-Fev-14 Parei aqui...
-# RESOLVER: usar resource compiler para compilar mdi.qrc em Qt5 (mdi_rc.py antigo
-# está em Qt4
-
 # Para chamar a ui simplezinha, ui_new_main_ui
 # from ui_new_main_ui import Ui_MainWindow
 
 # Para chamar a antiga e complexa interface, ui_main
 from ui_maininterface_with_toolbar import Ui_MainInterfaceClass
 
-# class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-class MainWindow(QtWidgets.QMainWindow, Ui_MainInterfaceClass):
+from ui_languages import Ui_LanguageDlg
+
+
+class MainWindow(QMainWindow, Ui_MainInterfaceClass):
 
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
+        # lang_dlg = Ui_LanguageDlg("Interface languages")
+        # lang_dlg.okButton.clicked()
+        # 2023-Fev-16 Defining custom signal/slots
+        # self.actionLanguage.triggered.connect(self.button_clicked)
+        self.actionLanguage.triggered.connect(self.actionLanguage_triggered)
 
-app = QtWidgets.QApplication(sys.argv)
+    def button_clicked(self, s):
+        dlg = QMessageBox(self)
+        dlg.setWindowTitle("I have a question!")
+        dlg.setText("This is a simple dialog")
+        button = dlg.exec_()
 
+        if button == QMessageBox.Ok:
+            print("OK!")
+
+    def actionLanguage_triggered(self, s):
+        # lang_dlg = Ui_LanguageDlg("Interface languages")
+        lang_dlg = Ui_LanguageDlg()
+        # lang_dlg. okButton.clicked()
+        lang_dlg.setupUi(QDialog)
+        # se_ok = lang_dlg.okButton.clicked()
+        # if se_ok == Ui_LanguageDlg
+
+        # dlg = QMessageBox(self)
+        # dlg.setWindowTitle("I have a question!")
+        # dlg.setText("This is a simple dialog")
+        # button = dlg.exec_()
+
+        # if button == QMessageBox.Ok:
+        #     print("OK!")
+
+app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
 app.exec_()
