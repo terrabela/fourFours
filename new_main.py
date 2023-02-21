b1f57d837b72ea42ce438084c35c22ae06e7049a
terrabela <maduar@vivaldi.net>
2023-02-20 21:31:59 -0300

    Implementing QTableViews

--- new_main.py
+++ new_main.py
@@ -6,10 +6,16 @@ from PySide2.QtWidgets import QApplication, QMainWindow, QMessageBox, QDialog
 
 # 2023-Fev-14
 # A organização dos tutoriais é algo confusa.
+# Introdućão:
+# https://www.pythonguis.com/pyside2/
 # O mais direto para uso com Qt Designer é
 # https://www.pythonguis.com/tutorials/pyside-first-steps-qt-designer/
+# Diálogos:
+# https://www.pythonguis.com/tutorials/pyside-dialogs/
 # Em seguida, para adicionar diálogos ao app:
 # https://www.pythonguis.com/tutorials/pyside-creating-dialogs-qt-designer/
+# Com numpy e Pandas:
+# https://www.pythonguis.com/tutorials/pyside-qtableview-modelviews-numpy-pandas/
 
 # Para chamar a ui simplezinha, ui_new_main_ui
 # from ui_new_main_ui import Ui_MainWindow
@@ -19,14 +25,25 @@ from ui_maininterface_with_toolbar import Ui_MainInterfaceClass
 
 from ui_languages import Ui_LanguageDlg
 
+
 class LngDlg(QDialog, Ui_LanguageDlg):
     def __init__(self):
         super(LngDlg, self).__init__()
         self.setupUi(self)
         self.setWindowTitle("Vamos customizar!!!")
+        # self.okButton.clicked.connect(self.printa())
+        self.okButton.clicked.connect(self.printa())
         # serah aqui?
         # Como fazer o connect?
         # https://www.pythonguis.com/tutorials/pyside-dialogs/
+        # self.Rejected.connect(self.escolhe()) ERRADO
+        # self.cancelButton.connect(self.cancelButton.clicked(), self.escolhe()) ERRADO
+
+    def printa(self):
+        print("Chamou")
+
+    def escolhe(self):
+        print("Escolhe")
 
 
 class MainWindow(QMainWindow, Ui_MainInterfaceClass):
@@ -41,24 +58,13 @@ class MainWindow(QMainWindow, Ui_MainInterfaceClass):
         # self.actionLanguage.triggered.connect(self.button_clicked)
         self.actionLanguage.triggered.connect(self.actionLanguage_triggered)
 
-    def button_clicked(self, s):
-        dlg = QMessageBox(self)
-        dlg.setWindowTitle("I have a question!")
-        dlg.setText("This is a simple dialog")
-        button = dlg.exec_()
-
-        if button == QMessageBox.Ok:
-            print("OK!")
-
     def actionLanguage_triggered(self, s):
         langdlg = LngDlg()
-        langdlg.exec_()
-        # AQUI? como usar ????
-        if langdlg.accept():
-            print("Accepted!")
-        if langdlg.Rejected:
-            print("Rejected!")
-        print("OK!")
+        if langdlg.exec_():
+            print("Success!")
+        else:
+            print("Cancel!")
+
 
 app = QApplication(sys.argv)
 window = MainWindow()
