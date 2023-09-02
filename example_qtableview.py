8f38584b42a3b150bf2edcdc8079e2f3d8d976e3
terrabela <maduar@vivaldi.net>
2023-09-02 11:04:32 -0300

    Implementing QTableViews with PySide2

--- example_qtableview.py
+++ example_qtableview.py
@@ -1,43 +1,42 @@
 import sys
 from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtCore import Qt
+import numpy as np
 
 
 class TableModel(QtCore.QAbstractTableModel):
+
     def __init__(self, data):
         super(TableModel, self).__init__()
         self._data = data
 
     def data(self, index, role):
         if role == Qt.DisplayRole:
-            # See below for the nested-list data structure.
-            # .row() indexes into the outer list,
-            # .column() indexes into the sub-list
-            return self._data[index.row()][index.column()]
+            # Note: self._data[index.row()][index.column()] will also work
+            value = self._data[index.row(), index.column()]
+            return str(value)
 
     def rowCount(self, index):
-        # The length of the outer list.
-        return len(self._data)
+        return self._data.shape[0]
 
     def columnCount(self, index):
-        # The following takes the first sub-list, and returns
-        # the length (only works if all rows are an equal length)
-        return len(self._data[0])
+        return self._data.shape[1]
 
 
 class MainWindow(QtWidgets.QMainWindow):
+
     def __init__(self):
         super().__init__()
 
         self.table = QtWidgets.QTableView()
 
-        data = [
-          [4, 9, 2],
-          [1, 0, 0],
-          [3, 5, 0],
+        data = np.array([
+          [1, 9, 2],
+          [1, 0, -1],
+          [3, 5, 2],
           [3, 3, 2],
-          [7, 8, 9],
-        ]
+          [5, 8, 9],
+        ])
 
         self.model = TableModel(data)
         self.table.setModel(self.model)
